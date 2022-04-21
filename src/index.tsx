4cb99b3327b9aade877fb937c90785dc0d5d6b36
Mariana-Usuga <Mariana-Usuga>
2022-04-20 20:24:10 -0500

    initial navbar

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import LandingPage from './pages/LandingPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import NavBar from '../components/NavBar';
+
+const LandingPage = () => (
+  <NavBar />
+);
+
+export default LandingPage;
