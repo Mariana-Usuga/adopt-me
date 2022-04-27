613ebbdbe00aaf7405fb49a869f6754ba4b37163
Mariana-Usuga <Mariana-Usuga>
2022-04-27 17:00:55 -0500

    better products dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -4,32 +4,24 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { FC, useState } from 'react';
-import { FaAngleDown, FaCat, FaDog } from 'react-icons/fa';
-// import ListProduct from '../ListProduct/ListProduct';
-import { DogProducts } from '../../Data/DogProducts';
-import { CatProducts } from '../../Data/CatProducts/CatProducts';
+import { FaAngleDown, FaCat, FaDog, FaAngleRight } from 'react-icons/fa';
 
-const Dropdown: FC<{ animal: string }> = ({ animal }) => {
-  // const [show, setShow] = useState(false);
-
+const Dropdown: FC<{ animal: string, faAngleDown:boolean }> = ({ animal, faAngleDown }) => {
   const products = {
     cat: <FaCat />,
     dog: <FaDog />,
   };
 
   return (
-    <>
-      <div className="category__item">
-        {animal === 'Dog' ? <FaDog /> : <FaCat />}
-        <div className="category__item__pet">
-          {animal}
-        </div>
-        <span className="category__item__down">
-          <FaAngleDown />
-        </span>
+    <div className="category__item">
+      {animal === 'Dog' ? <FaDog /> : <FaCat />}
+      <div className="category__item__pet">
+        {animal}
       </div>
-      {/* {show && <ListProduct products={DogProducts} />} */}
-    </>
+      <span className="category__item__down">
+        {!faAngleDown ? <FaAngleRight /> : <FaAngleDown /> }
+      </span>
+    </div>
   );
 };
 
--- src/components/NavBarSecondary/NavBarSecondary.tsx
+++ src/components/NavBarSecondary/NavBarSecondary.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-lonely-if */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable quotes */
@@ -6,7 +8,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import { useState } from 'react';
+import { useState, MouseEvent } from 'react';
 import Dropdown from '../Dropdown/Dropdown';
 import { DogProducts } from '../../Data/DogProducts';
 import { CatProducts } from '../../Data/CatProducts/CatProducts';
@@ -16,7 +18,7 @@ import './navBarSecondary.scss';
 
 const NavBarSecondary = () => {
   const [animalProduct, setAnimalProduct] = useState<number>(0);
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState({ cat: false, dog: false });
 
   const products = [
     [],
@@ -24,26 +26,36 @@ const NavBarSecondary = () => {
     CatProducts,
   ];
 
-  const showProducts = (index:number) => {
+  const showProducts = (e:MouseEvent<HTMLButtonElement>, index:number) => {
     setAnimalProduct(index);
-    setShow(!show);
+    // if (index === 1) {
+    const { name } = e.currentTarget;
+    console.log('showw', name);
+    setShow({ ...show, cat: !show.cat, dog: false });
+    // } else {
+    // setShow({ ...show, cat: !show.cat, dog: false });
+    // }
   };
 
   return (
     <>
       <div className="category">
-        <div onClick={() => showProducts(1)}>
-          <Dropdown animal="Dog" />
-        </div>
-        <div onClick={() => showProducts(2)}>
-          <Dropdown animal="Cat" />
-        </div>
+        <button type="button" name="dog" onClick={(e) => showProducts(e, 1)}>
+          <Dropdown animal="Dog" faAngleDown={show.dog} />
+        </button>
+        <button type="button" name="cat" onClick={(e) => showProducts(e, 2)}>
+          <Dropdown animal="Cat" faAngleDown={show.cat} />
+        </button>
       </div>
-      <div className={!show ? "category__listProduct" : "category__listProduct--show"}>
+      {/* <div className={!show ? "category__listProduct" : "category__listProduct--show"}>
         <ListProduct products={products[animalProduct]} />
-      </div>
+      </div> */}
+      {show.cat || show.dog ? <ListProduct products={products[animalProduct]} /> : null}
     </>
   );
 };
 
 export default NavBarSecondary;
+function e(e: any, arg1: number): void {
+  throw new Error('Function not implemented.');
+}
