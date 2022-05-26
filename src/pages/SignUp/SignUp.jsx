4229a7ae553b51fbbe04f9eaee91d40ec017f1d8
Mariana-Usuga <Mariana-Usuga>
2022-05-26 17:04:09 -0500

    initial sign up

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { useState } from 'react';
+import { Formik } from 'formik';
+
+const SignUp = () => {
+  const [inputName, cambiarInputName] = useState('');
+  const [inputEmail, cambiarInputEmail] = useState('');
+  const [inputPassword, cambiarInputPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Formulario Enviado!');
+  };
+
+  const handleInputName = (e) => {
+    cambiarInputName(e.target.value);
+  };
+
+  const handleInputEmail = (e) => {
+    cambiarInputEmail(e.target.value);
+  };
+
+  const handleInputPassword = (e) => {
+    cambiarInputPassword(e.target.value);
+  };
+
+  return (
+    <Formik
+      onSubmit={() => {
+        console.log('eviado');
+      }}
+    >
+      {() => (
+        <form action="" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Nombre:</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              value={inputName}
+              onChange={handleInputName}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              id="email"
+              type="text"
+              name="email"
+              value={inputEmail}
+              onChange={handleInputEmail}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value={inputPassword}
+              onChange={handleInputPassword}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </Formik>
+  );
+};
+
+export default SignUp;
