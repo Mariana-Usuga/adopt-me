5f6e7bee9c62baf29df50f32fa851909fdae31ca
Mariana-Usuga <Mariana-Usuga>
2022-07-21 11:25:36 -0500

    better shopping cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,36 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './shoppingCart.scss';
 
 const Cart = () => {
   // const token = JSON.parse(localStorage.getItem('token'));
   // const cart = useSelector((state) => state.cartReducer.cart);
-  const [isEmpty] = useState(true);
+  // const [isEmpty] = useState(true);
   // useEffect(() => {
   //   if (cart.length) {
   //     setIsEmpty(false);
   //   }
   // }, []);
+  console.log('bien');
   return (
-    <>
-      <main id="shopping-cart">
-        {/* <ShoppingCart /> */}
-        {/* <CartSummary /> */}
-      </main>
-      {/* <Link
-        to={token ? '/pages/paymentProcess' : '/login'}
-        style={{ textDecoration: 'none' }}
-      > */}
-      <button
-          // onClick={buyShoppingCart}
-        className="shopping-cart__btnPay"
-        type="button"
-        disabled={isEmpty}
-      >
-        Proceder al Pago
-      </button>
-      {/* </Link> */}
-    </>
+    <div className="cart">
+      <div className="cart__header">Carrito</div>
+      <div className="cart__shoppingCart">
+        <div className="cart__shoppingCart__product">
+          <div className="cart__shoppingCart__product__container">
+            <div className="cart__shoppingCart__product__img">
+              <img src="https://laika.com.co/cdn-cgi/image/onerror=redirect,format=auto,fit=scale-down,width=600,quality=80/https://laikapp.s3.amazonaws.com/images_products/2652_Evolve_Cat_%2ALata_Pavo__614X615.png" alt="" />
+            </div>
+            <div className="cart__shoppingCart__product__detail">
+              <div className="cart__shoppingCart__product__detail__title">Evolve Cat Lata Pavo</div>
+              <div className="cart__shoppingCart__product__detail__totalProduct">
+                <div className="cart__shoppingCart__product__detail__totalProduct__totalPrice">
+                  20.000
+                </div>
+                <div className="cart__shoppingCart__product__detail__totalProduct__amount">
+                  <span className="cart__shoppingCart__product__detail__totalProduct__amount__less">-</span>
+                  <span className="cart__shoppingCart__product__detail__totalProduct__amount__number">2</span>
+                  <span className="cart__shoppingCart__product__detail__totalProduct__amount__plus">+</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 export default Cart;
