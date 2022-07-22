9f59b7f21f18245603bcd7e14a3be0bb3d13b1b1
Mariana-Usuga <Mariana-Usuga>
2022-07-22 16:05:06 -0500

    create route in item detail

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,29 +1,25 @@
 /* eslint-disable import/extensions */
 // eslint-disable-next-line import/no-unresolved, import/extensions
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // eslint-disable-next-line import/no-unresolved
 import { products } from '../../Data/products';
 
 import './featured.scss';
 
-const Featured = () => {
-  const { id } = useParams();
-
-  return (
-    <div className="featuredProducts">
-      <div className="featuredProducts__title">Doggy Must Haves</div>
-      <div className="featuredProducts__container">
-        {products.map((product) => (
-          <Link to="pages/itemDetail">
-            <div className="featuredProducts__container__item">
-              <img className="featuredProducts__container__item__img" src={product.img} alt="" />
-              <div className="featuredProducts__container__item__name">{product.name}</div>
-            </div>
-          </Link>
-        )) }
-      </div>
+const Featured = () => (
+  <div className="featuredProducts">
+    <div className="featuredProducts__title">Doggy Must Haves</div>
+    <div className="featuredProducts__container">
+      {products.map((product) => (
+        <Link to={`pages/itemDetail/${product.id}`}>
+          <div className="featuredProducts__container__item">
+            <img className="featuredProducts__container__item__img" src={product.img} alt="" />
+            <div className="featuredProducts__container__item__name">{product.name}</div>
+          </div>
+        </Link>
+      )) }
     </div>
-  );
-};
+  </div>
+);
 
 export default Featured;
--- src/pages/ItemDetail/ItemDetail.tsx
+++ src/pages/ItemDetail/ItemDetail.tsx
@@ -4,54 +4,56 @@ import { addToCart } from '../../store/actions/cartActions';
 // import { products } from '../../mockData/products';
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import Amount from '../../components/Amount/Amount';
+import { Product } from '../../interface/Product';
+import { products } from '../../Data/products';
 
 import './itemDetail.scss';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ItemDetail = () => {
   const dispatch = useDispatch();
-  // /* eslint-disable */
-  // const handleCarrito = () => {
-  //   const cartPrev = JSON.parse(localStorage.getItem('cartProduct')) || [];
-  //   const exists = cartPrev.find((element) => element._id === product._id);
-  //   if (exists) {
-  //     dispatch(addQtyProductToCart(exists));
-  //   } else {
-  //     dispatch(addProductToCart(product));
-  //   }
-  // };
-  console.log('working');
+  const {id} = useParams()
+
   const addProduct = () => {
     dispatch(addToCart());
   };
+
   return (
     <div className="itemDetail">
-      <div className="itemDetail__main">
-        <img
-          className="itemDetail__main__img"
-          alt="product"
-          src="https://res.cloudinary.com/db3njhxi0/image/upload/v1651706826/wevcdgyshtxkeyfbjkyg.jpg"
-        />
-      </div>
-      <div className="itemDetail__info">
-        <h2 className="itemDetail__info__title">Cat Chow for Cat</h2>
-        <div className="itemDetail__info__container">
-          <div>20.000</div>
-          <Amount />
-        </div>
-        <div className="itemDetail__info__btns">
-          <button className="itemDetail__info__btns__btnBuy" type="button">
-            Comprar
-          </button>
-          <button
-            className="itemDetail__info__btns__btnAddCart"
-            type="button"
-            onClick={addProduct}
-          >
-            Añadir al carrito
-          </button>
-        </div>
-      </div>
+       {products.find((p: Product) => p.id === id).map((pr) => (
+    <li>
+      {pr}
+    </li>
+  ))}
     </div>
   );
 };
 export default ItemDetail;
+<>
+<div className="itemDetail__main">
+<img
+  className="itemDetail__main__img"
+  alt="product"
+  src="https://res.cloudinary.com/db3njhxi0/image/upload/v1651706826/wevcdgyshtxkeyfbjkyg.jpg"
+/>
+</div>
+<div className="itemDetail__info">
+<h2 className="itemDetail__info__title">Cat Chow for Cat</h2>
+<div className="itemDetail__info__container">
+  <div>20.000</div>
+  <Amount />
+</div>
+<div className="itemDetail__info__btns">
+  <button className="itemDetail__info__btns__btnBuy" type="button">
+    Comprar
+  </button>
+  <button
+    className="itemDetail__info__btns__btnAddCart"
+    type="button"
+    onClick={addProduct}
+  >
+    Añadir al carrito
+  </button>
+</div>
+</div>
