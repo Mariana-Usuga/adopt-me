717713f4827528f5365a82831991c839a522d9ee
Mariana-Usuga <Mariana-Usuga>
2022-07-25 14:13:07 -0500

    fix item detail route

--- src/pages/ItemDetail/ItemDetail.tsx
+++ src/pages/ItemDetail/ItemDetail.tsx
@@ -1,59 +1,73 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { addToCart } from '../../store/actions/cartActions';
 // import { products } from '../../mockData/products';
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import Amount from '../../components/Amount/Amount';
+// eslint-disable-next-line import/no-unresolved, import/extensions, no-unused-vars
 import { Product } from '../../interface/Product';
+// eslint-disable-next-line import/no-unresolved, import/extensions
 import { products } from '../../Data/products';
 
 import './itemDetail.scss';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
 
 const ItemDetail = () => {
-  const dispatch = useDispatch();
-  const {id} = useParams()
+  // const dispatch = useDispatch();
+  const { id } = useParams();
+  const [item, setItem] = useState({
+    id: 0,
+    img: '',
+    name: '',
+    price: 0,
+  });
 
   const addProduct = () => {
-    dispatch(addToCart());
+    // dispatch(addToCart());
+    console.log('pr', item);
   };
+
+  useEffect(() => {
+    console.log('useefe');
+    const p = products.find((product: Product) => product.id === Number(id));
+    if (p !== undefined) {
+      setItem(p);
+    }
+    // console.log('useEffe', p, 'id para', id, 'pro');
+  // setItem(products.find((p: Product) => p.id === id));
+  }, []);
 
   return (
     <div className="itemDetail">
-       {products.find((p: Product) => p.id === id).map((pr) => (
-    <li>
-      {pr}
-    </li>
-  ))}
+      <div className="itemDetail__main">
+        <img
+          className="itemDetail__main__img"
+          alt="product"
+          src={item.img}
+        />
+      </div>
+      <div className="itemDetail__info">
+        <h2 className="itemDetail__info__title">{item.name}</h2>
+        <div className="itemDetail__info__container">
+          <div>{item.price}</div>
+          <Amount />
+        </div>
+        <div className="itemDetail__info__btns">
+          <button className="itemDetail__info__btns__btnBuy" type="button">
+            Comprar
+          </button>
+          <button
+            className="itemDetail__info__btns__btnAddCart"
+            type="button"
+            onClick={addProduct}
+          >
+            Añadir al carrito
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 export default ItemDetail;
-<>
-<div className="itemDetail__main">
-<img
-  className="itemDetail__main__img"
-  alt="product"
-  src="https://res.cloudinary.com/db3njhxi0/image/upload/v1651706826/wevcdgyshtxkeyfbjkyg.jpg"
-/>
-</div>
-<div className="itemDetail__info">
-<h2 className="itemDetail__info__title">Cat Chow for Cat</h2>
-<div className="itemDetail__info__container">
-  <div>20.000</div>
-  <Amount />
-</div>
-<div className="itemDetail__info__btns">
-  <button className="itemDetail__info__btns__btnBuy" type="button">
-    Comprar
-  </button>
-  <button
-    className="itemDetail__info__btns__btnAddCart"
-    type="button"
-    onClick={addProduct}
-  >
-    Añadir al carrito
-  </button>
-</div>
-</div>
